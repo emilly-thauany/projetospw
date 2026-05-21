b086f78035459213353faf8eea4b4ebe1c92e213
emilly-thauany <emilly.11493342@aluno.mg.gov.br>
2026-05-21 13:29:01 -0300

    Add files via upload

---
+++ projetospw/questao 3.php
@@ -0,0 +1,24 @@
+!DOCTYPE html>
+<html lang="pt-br"><
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questao 3</title>
+</head>
+<body>
+    <h1> queatao 3/</title>
+    
+</body>
+</html>
+     <?php
+ $numero=8; 
+    if($numero%2==0){
+    echo "6 numero e par";
+    }
+  else{
+     echo "6 numero e impar";
+ }
+
+ ?>
+ 
+
---
+++ projetospw/questao 6.php
@@ -0,0 +1,17 @@
+!DOCTYPE html>
+<html lang="pt-br"><
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questao 6</title>
+</head>
+<body>
+    <h1> queatao 6/</title>
+    <?php
+$numeros =[5,10,15,20,25];
+Foreach($numeros as $numeros){
+        if($numero>10);{ 
+            echo"$numero e maior que 10<br";
+        }
+}
+?>
